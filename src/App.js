f5da97ee0881991d0837b8b5952a33974927d19a
EC2 Default User <ec2-user@ip-172-31-52-63.ec2.internal>
2021-02-27 07:27:18 +0000

    setup for buyers page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from 'react-dom';
+import Buyers from "./Buyers";
 import './App.css';
 
+
 function App() {
+  function handleOrders(){
+    ReactDOM.render(<Buyers />,document.getElementById('root'));
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src="https://scontent.fewr1-5.fna.fbcdn.net/v/t1.0-9/131111442_101471758510914_2141520648034571401_n.png?_nc_cat=105&ccb=3&_nc_sid=09cbfe&_nc_ohc=pl2djSfZQ38AX9AS7v1&_nc_ht=scontent.fewr1-5.fna&oh=0a910506d54e3fbf78dbfbacad55148c&oe=605F2D53" />
+    <h1>Welcome To Ecommerce Fundraising Platform</h1>
+    <div className="sidebar">
+    <button>Home</button>
+    <form onSubmit={handleOrders}>
+      <button>Orders</button>
+    </form>
+    </div> 
     </div>
   );
 }
