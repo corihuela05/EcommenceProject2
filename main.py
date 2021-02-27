21b956f327b8af2618531adcb05ee7837d28206b
Lily Concepcion <lilyconcepcion123@gmail.com>
2021-02-27 00:35:51 -0500

    added git config

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import flask
+from flask_mysqldb import MySQL
 
 app = flask.Flask("__main__")
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'gmfdatabase'
+
+@app.route("/insertbuyer")
 
 @app.route("/")
 def my_index():
