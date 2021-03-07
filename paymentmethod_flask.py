0c99c5e70952383a4e60c5ce73f4ae5b0d8d6355
corihuela05 <58125836+corihuela05@users.noreply.github.com>
2021-03-07 18:08:43 -0500

    Update paymentmethod_flask.py

--- paymentmethod_flask.py
+++ paymentmethod_flask.py
@@ -9,7 +9,9 @@ app.config['MYSQL__ER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'gmfdatabase'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/gmfdatabase'
-#app.config['sqlarchemy_track_modifications']=False
+app.config['sqlarchemy_track_modifications']=False
+db = MySQL(app)
+
 '''
 db = MySQL(app)
 db.connect()
@@ -17,24 +19,43 @@ cur = db.connection.cursor()
 '''
 db = SQLAlchemy(app)
 
-'''
+
 ### adds table, just for testing connection ###
-class paymentmethods (db.Model):
-    ID_methodtype = db.Column(db.varchar(5), primary_key=True)
-    Description_payment = db.Column(db.text(50), unique=True)
-    def __init__(self, id-type, Description_usertype):
-        self.methodtype = id_type
-        self.description_payment = Description_type
+class paymentmethod (db.Model):
+    Id_Number_paymentmethod = db.Column(db.String(50), primary_key=True)
+    Fullname_paymentmethod = db.Column(db.String(50), primary_key=True)
+    CardNum_paymentmethod = db.Column(db.String(16), unique=True)
+    ExpDate_payment= db.column(db.date(8), unique=True)
+    CRV_paymentmethod= db.column(db.Integer(3), unique=True)
+    def __init__(self, Id_Number_paymentmethod,Fulllname_paymentmethod, CardNum_paymentmethod, ExpDate_payment , CRV_paymentmethod):
+        self.Id_Number_paymentmethod = Id_Number_paymentmethod
+        self.Fulllname_paymentmethod = ID_Fulllname_paymentmethod
+        self.CardNum_paymentmethod = CardNum_paymentmethod
+        self.ExpDate_payment=ExpDate_payment
+        self.CRV_paymentmethod=CRV_paymentmethod
 db.create_all()
-db.session.add(paymentmethods(id_method_pay= "card" )) 
-b.session.add(paymentmethods(description= "American Express credit card" ))
-'''
-def remove_entry(db.model):
-    db.session.delete(ID_type)
+#Entry 1
+
+entry1=(Id_Number_paymentmethod = 1, Fullname_paymentmethod="Crystal Rodriguez", CardNum_paymentmethod=1234566770001234556, ExpDate_payment=10/20/21, CRV_paymentmethod=123)
+db.session.commit()
+
+#Entry 2
+entry2= (Id_Number_paymentmethod 2, ID_Fulllname_paymentmethod. a="James Rodriguez", CardNum_paymentmethod= 10245677851253333)
+4/20/21, CRV_Paymentmethod=423)
+db.session.commit()
+
+
+
+def delete_entry(db.model):
+    db.session.delete(Id_Number_paymentmethod=1)
     db.session.commit()
 
+def modify_entry ():
+    db.session.update(user= User.query.filter_by(Id_Number_paymentmethod=2).first()
+    db.commit()
+
 @app.route("/")
-def my_index():
+def my_index():12
         return flask.render_template("index.html", token= "Hello")
 
 app.run(debug=True) 
