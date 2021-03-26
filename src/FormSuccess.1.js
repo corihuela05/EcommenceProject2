661bdfbba3d6fe62f01e2bc626f2271ada275c4a
sarahjlfoster <55552844+sarahjlfoster@users.noreply.github.com>
2021-03-26 12:12:58 -0400

    Update FormSuccess.1.js

--- src/FormSuccess.1.js
+++ src/FormSuccess.1.js
@@ -3,42 +3,47 @@ import ReactDOM from "react-dom";
 import "./Form.css";
 
 import Nonprofit from "./Nonprofit";
+import App from "./App";
+
 
 const FormSuccess = () => {
-  useEffect(() => {
-    redirectUser();
-  }, []);
+    useEffect(() => {
+        redirectUser();
+    }, []);
 
-  const redirectUser = () => {
-    const userType = localStorage.getItem("userType");
+    const redirectUser = () => {
+        const userType = localStorage.getItem("userType");
 
-    setTimeout(() => {
-      switch (userType) {
-        case "admin":
-          break;
-        case "nonprofit":
-          ReactDOM.render(<Nonprofit />, document.getElementById("root"));
-          break;
-        default:
-          break;
-      }
-    }, 1000);
+        setTimeout(() => {
+            switch (userType) {
+                case "admin":
+                    break;
+                case "nonprofit":
+                    ReactDOM.render(<Nonprofit />, document.getElementById("root"));
+                    break;
+                case "buyer":
+                    ReactDOM.render(<App />, document.getElementById("root"));
+                    break;
+                default:
+                    break;
+            }
+        }, 1000);
 
-    /*
-      admin -> Sellers
-      buyers
-      nonprofit -> dashboard?
-    */
-  };
+        /*
+          admin -> Sellers
+          buyers
+          nonprofit -> dashboard?
+        */
+    };
 
-  return (
-    <div className="form-content-right">
-      <h1 className="form-success">
-        You are now logged in and it will be redirected{" "}
-      </h1>
-      <img className="form-img-2" src="img/img-3.svg" alt="success-image" />
-    </div>
-  );
+    return (
+        <div className="form-content-right">
+            <h1 className="form-success">
+                You are now logged in and it will be redirected{" "}
+            </h1>
+            <img className="form-img-2" src="img/img-3.svg" alt="success-image" />
+        </div>
+    );
 };
 
 export default FormSuccess;
