3bbdef9caf1421e16cde76f848caf6b137bb5df5
sarahjlfoster <55552844+sarahjlfoster@users.noreply.github.com>
2021-03-26 11:01:29 -0400

    Update FormSuccess.1.js

--- src/FormSuccess.1.js
+++ src/FormSuccess.1.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import './Form.css';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./Form.css";
+
+import Nonprofit from "./Nonprofit";
 
 const FormSuccess = () => {
+  useEffect(() => {
+    redirectUser();
+  }, []);
+
+  const redirectUser = () => {
+    const userType = localStorage.getItem("userType");
+
+    setTimeout(() => {
+      switch (userType) {
+        case "admin":
+          break;
+        case "nonprofit":
+          ReactDOM.render(<Nonprofit />, document.getElementById("root"));
+          break;
+        default:
+          break;
+      }
+    }, 1000);
+
+    /*
+      admin -> Sellers
+      buyers
+      nonprofit -> dashboard?
+    */
+  };
+
   return (
-    <div className='form-content-right'>
-      <h1 className='form-success'>You are now logged in and it will be redirected </h1>
-      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+    <div className="form-content-right">
+      <h1 className="form-success">
+        You are now logged in and it will be redirected{" "}
+      </h1>
+      <img className="form-img-2" src="img/img-3.svg" alt="success-image" />
     </div>
   );
 };
