ebc5d14ac770ea094e64c81f0986f472ae714364
sarahjlfoster <55552844+sarahjlfoster@users.noreply.github.com>
2021-04-04 18:46:12 -0400

    Update CAPrivacyRights.js

--- src/CAPrivacyRights.js
+++ src/CAPrivacyRights.js
@@ -18,209 +18,210 @@ import School from './Schools';
 
 
 
-function CAPrivacyRights(){
-  function handlebeautyProducts(){
-    ReactDOM.render(<BeautyProducts />,document.getElementById('root'));
-  }
-  
-  function handleHomeandfurnitures(){
-  ReactDOM.render(<Homeandfurnitures />,document.getElementById('root'));
- }
+function CAPrivacyRights() {
+    function handlebeautyProducts() {
+        ReactDOM.render(<BeautyProducts />, document.getElementById('root'));
+    }
+
+    function handleHomeandfurnitures() {
+        ReactDOM.render(<Homeandfurnitures />, document.getElementById('root'));
+    }
 
 
-  function handleHome(){
-    ReactDOM.render(<Home />,document.getElementById('root'));
-  }
-  function handleProduct(){
-    ReactDOM.render(<Product />,document.getElementById('root'));
-  }
-  function handleShop(){
-    ReactDOM.render(<Shop />,document.getElementById('root'));
-  }
-  function handleCart(){
-    ReactDOM.render(<Cart />,document.getElementById('root'));
-  }
-   function handleNonprofit(){
-    ReactDOM.render(<Nonprofit />,document.getElementById('root'));
-  }
-  
-   function handleSchoolst(){
-    ReactDOM.render(<School />,document.getElementById('root'));
-  }
-  
-  
-    function handleAccount(){
-    ReactDOM.render(<Account />,document.getElementById('root'));
-  }
-   function handleuserlogin(){
-    ReactDOM.render(<FormSignup />,document.getElementById('root'));
-  }
-  function handleProfile(){
-    ReactDOM.render(<Profile />,document.getElementById('root'));
-  }
-  
-    function handlevideogame(){
-     ReactDOM.render(<Videog />,document.getElementById('root'));
-  }
-  
-  
-  return (
-    <div className="App">
-    <div className="Appheader">
-    <img src="logo.png" alt='company logo' className="companylogo" />
-    <img src="https://media1.tenor.com/images/3ced764a2cb7ad33ddf2145edb9904ae/tenor.gif?itemid=4320892" alt='flag' className="flag" />
-    <input type="text" name="search" placeholder="Search.."/>
-    <form onSubmit={handleHome}>
-      <button id="homebutton">Home</button>
-    </form>
-   
-  <form onSubmit={handleAccount}>
-      <button id="info1">Sign Up</button>
-    </form>
-    <form onSubmit={handleuserlogin}>
-      <button id="info">Login</button>
-    </form>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
-    <form onSubmit={handleCart}>
-      <button id="cart"><span className="glyphicon glyphicon-shopping-cart"></span>  Your Cart</button>
-    </form>
-    <div className="homepagesidebar">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <input type="checkbox" id="check"/>
-    <label for="check">
-      <i class="fa fa-bars" id="openbutton"></i>
-      <i class="fa fa-close" id="cancelbutton"></i>
-    </label>
-    <div className="sidebar">
-      <form onSubmit={handleHome}>
-        <button>Home</button>
-      </form>
-      <form onSubmit={handleProduct}>
-        <button>Products</button>
-      </form>
-      <form onSubmit={handleProfile}>
-        <button>Profile</button>
-      </form>
-      <form onSubmit={handleShop}>
-        <button>Shop</button>
-      </form>
-      <form onSubmit={handleNonprofit}>
-      <button>Non Profits</button>
-    </form>
-    <button>Buy Again</button>
-    <button>List</button>
-    <button>Registry</button>
-    <button>Gift Finder</button>
-    <button>Gift Card</button>
-    <button>Black Owned Businesses</button>
-    <button>Women Owned Businesses</button>
-    <button>Help</button>
-    <button>Departments</button>
-    <button>Electronics & Office</button>
-    <button>Clothing, Shoes, & Accessories</button>
-    
-      <form onSubmit={handleHomeandfurnitures}>
-       <button>Home, Furniture & Appliances</button>
-    </form>
-   
-    <button>Toys, Games and Video Games</button>
-    <button>Home Improvement</button>
-    <button>Movies, Music & Books</button>
-    <button>Patio & Garden</button>
-    <button>Food</button>
-    <button>Pets</button>
-    <button>Pharmacy, Health & Personal Care</button>
-    <button>Beauty</button>
-    <button>Sports, Fitness & Outdoors</button>
-    <button>Auto & Tire</button>
-    <button>Photos</button>
-    <button>Art, Craft, Sewing & Party Supplies</button>
-    </div> 
-    </div>
-    <div className="tabs">
-      <button>Best Sellers</button>
-      <button>Today's Deals</button>
-      <button>Customer Service</button>
-      <button>New Releases</button>
-      <button>Near Me</button>
-      <button>Books</button>
-      <button>Fashion</button>
-      <button>Gift Cards</button>
-    </div>
-    </div>
-    <div className="sliderslide">
-    <div className="slider">
-    
-    
-      <h1>CA Privacy Rights </h1>
-           <p>
+    function handleHome() {
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+    function handleProduct() {
+        ReactDOM.render(<Product />, document.getElementById('root'));
+    }
+    function handleShop() {
+        ReactDOM.render(<Shop />, document.getElementById('root'));
+    }
+    function handleCart() {
+        ReactDOM.render(<Cart />, document.getElementById('root'));
+    }
+    function handleNonprofit() {
+        ReactDOM.render(<Nonprofit />, document.getElementById('root'));
+    }
+
+    function handleSchoolst() {
+        ReactDOM.render(<School />, document.getElementById('root'));
+    }
 
 
-What Data We Collect
-The personal information we process about you and how we collect it can vary depending on (a) the product(s) or service(s) you use; (b) how you use the product or service; (c) how you have interacted with OpenText, and (d) the information that we have obtained from a customer or from a third party with permission to share such information with us.
-OpenText may collect personal information about you such as, for example: full name, email address, telephone number, company name, company location, position in company, department, billing information including credit card details where applicable, IP address and how you use our services. See Cookie Technology section for details on what we collect using cookies and other technologies.
-OpenText may combine the personal information you provide with information that is not collected directly from you. Data not provided directly by you may be supplied by OpenText’s vendors and/or partners. Refer to Personal Information Collected from Third Parties for additional information.
-How We Process Your Data
-OpenText processes personal information to respond to your requests, to provide you OpenText products and services, including information, support and delivery and access to online resources. Some of the specific processing activities include, for example:
+    function handleAccount() {
+        ReactDOM.render(<Account />, document.getElementById('root'));
+    }
+    function handleuserlogin() {
+        ReactDOM.render(<FormSignup />, document.getElementById('root'));
+    }
+    function handleProfile() {
+        ReactDOM.render(<Profile />, document.getElementById('root'));
+    }
+
+    function handlevideogame() {
+        ReactDOM.render(<Videog />, document.getElementById('root'));
+    }
+
+
+    return (
+        <div className="App">
+            <div className="Appheader">
+                <img src="logo.png" alt='company logo' className="companylogo" />
+                <img src="https://media1.tenor.com/images/3ced764a2cb7ad33ddf2145edb9904ae/tenor.gif?itemid=4320892" alt='flag' className="flag" />
+                <input type="text" name="search" placeholder="Search.." />
+                <form onSubmit={handleHome}>
+                    <button id="homebutton">Home</button>
+                </form>
+
+                <form onSubmit={handleAccount}>
+                    <button id="info1">Sign Up</button>
+                </form>
+                <form onSubmit={handleuserlogin}>
+                    <button id="info">Login</button>
+                </form>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" />
+                <form onSubmit={handleCart}>
+                    <button id="cart"><span className="glyphicon glyphicon-shopping-cart"></span>  Your Cart</button>
+                </form>
+                <div className="homepagesidebar">
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                    <input type="checkbox" id="check" />
+                    <label for="check">
+                        <i class="fa fa-bars" id="openbutton"></i>
+                        <i class="fa fa-close" id="cancelbutton"></i>
+                    </label>
+                    <div className="sidebar">
+                        <form onSubmit={handleHome}>
+                            <button>Home</button>
+                        </form>
+                        <form onSubmit={handleProduct}>
+                            <button>Products</button>
+                        </form>
+                        <form onSubmit={handleProfile}>
+                            <button>Profile</button>
+                        </form>
+                        <form onSubmit={handleShop}>
+                            <button>Shop</button>
+                        </form>
+                        <form onSubmit={handleNonprofit}>
+                            <button>Non Profits</button>
+                        </form>
+                        <button>Buy Again</button>
+                        <button>List</button>
+                        <button>Registry</button>
+                        <button>Gift Finder</button>
+                        <button>Gift Card</button>
+                        <button>Black Owned Businesses</button>
+                        <button>Women Owned Businesses</button>
+                        <button>Help</button>
+                        <button>Departments</button>
+                        <button>Electronics & Office</button>
+                        <button>Clothing, Shoes, & Accessories</button>
+
+                        <form onSubmit={handleHomeandfurnitures}>
+                            <button>Home, Furniture & Appliances</button>
+                        </form>
+
+                        <button>Toys, Games and Video Games</button>
+                        <button>Home Improvement</button>
+                        <button>Movies, Music & Books</button>
+                        <button>Patio & Garden</button>
+                        <button>Food</button>
+                        <button>Pets</button>
+                        <button>Pharmacy, Health & Personal Care</button>
+                        <button>Beauty</button>
+                        <button>Sports, Fitness & Outdoors</button>
+                        <button>Auto & Tire</button>
+                        <button>Photos</button>
+                        <button>Art, Craft, Sewing & Party Supplies</button>
+                    </div>
+                </div>
+                <div className="tabs">
+                    <button>Best Sellers</button>
+                    <button>Today's Deals</button>
+                    <button>Customer Service</button>
+                    <button>New Releases</button>
+                    <button>Near Me</button>
+                    <button>Books</button>
+                    <button>Fashion</button>
+                    <button>Gift Cards</button>
+                </div>
+            </div>
+            <div className="sliderslide">
+                <div className="slider">
+
+
+                    <h1>CA Privacy Rights </h1>
+                    <b>What Data We Collect</b>
+                        <p>The personal information we process about you and how we collect
+                        it can vary depending on (a) the product(s) or service(s) you use;
+                        (b) how you use the product or service; (c)
+                        how you have interacted with OpenText, and (d)
+                        the information that we have obtained from
+                        a customer or from a third party with permission to share
+                        such information with us.
+
+                        OpenText may collect personal information about you such as, for example:
+                        full name, email address, telephone number, company name, company location,
+                        position in company, department, billing information including credit card
+                        details where applicable, IP address and how you use our services.
+                        See Cookie Technology section for details on what we collect
+                        using cookies and other technologies.
+
+                        </p>
+
+                        <div className="dot">
+
+                            <div>
 
 
 
 
-</p>
-      
-      <div className="dot">
-      
-    <div>
-    
-    
-      
- 
-      
-    </div>
-    
-    
-  
 
-      
-      </div>
+                            </div>
+
+
+
+
+
+                        </div>
     </div>
-  
-    
-    
-    <footer class="site-footer">
-    <h1>About Greyson</h1>
-    <div className="footer1">
-      <button><img src="logo.png" alt='logo' width="150" height="150"/>1717 Harrison St. Newark, NJ 07028,USA</button>
-    </div>
-    <div className="footer2">
-     <button>OUR COMPANY</button>
-      <button>VENDOR DIRECTORY</button>
-      <button>HELP CENTER</button>
-      <button>COSTUMER SERVICE</button>
-    </div>
-    <div className="footer3">
-      <button>RETURNS</button>
-      <button>PRODUCT RECALLS</button>
-      <button>ACCESSIBILTY</button>
-    </div>
-    <div className="footer4">
-      <button>TERMS OF SERVICE</button>
-      <button>PRIVACY & SECURITY</button>
-      <button>CA Privacy Rights</button>
-      <button>Do Not Sell My information</button>
-      <button>Request My information</button>
-      <button>Tax Exempt Program</button>
-    </div>
-    <div className="footer5">
-      <hr/>
-      <p> Copyright © 2021 All Rights Reserved by Greyson</p>  
-    </div>
-    </footer>
-    </div>
-    </div>
+
+
+
+                    <footer class="site-footer">
+                        <h1>About Greyson</h1>
+                        <div className="footer1">
+                            <button><img src="logo.png" alt='logo' width="150" height="150" />1717 Harrison St. Newark, NJ 07028,USA</button>
+                        </div>
+                        <div className="footer2">
+                            <button>OUR COMPANY</button>
+                            <button>VENDOR DIRECTORY</button>
+                            <button>HELP CENTER</button>
+                            <button>COSTUMER SERVICE</button>
+                        </div>
+                        <div className="footer3">
+                            <button>RETURNS</button>
+                            <button>PRODUCT RECALLS</button>
+                            <button>ACCESSIBILTY</button>
+                        </div>
+                        <div className="footer4">
+                            <button>TERMS OF SERVICE</button>
+                            <button>PRIVACY & SECURITY</button>
+                            <button>CA Privacy Rights</button>
+                            <button>Do Not Sell My information</button>
+                            <button>Request My information</button>
+                            <button>Tax Exempt Program</button>
+                        </div>
+                        <div className="footer5">
+                            <hr />
+                            <p> Copyright © 2021 All Rights Reserved by Greyson</p>
+                        </div>
+                    </footer>
+                </div>
+            </div>
   );
 }
 
 export default CAPrivacyRights;
-
-    
